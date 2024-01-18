b82183deaf08b7318f6d182f5ff2923000fe305b
Rasheed Jahjah <rasheed.jahjah@rds.de>
2024-01-18 10:11:49 +0100

    Thumbnail update

--- SPOSiteDesign.ps1
+++ SPOSiteDesign.ps1
@@ -2,10 +2,19 @@ $Title = "RDS Custom Site Template - Demo"
 $Description = "Site Design & Site Script to provision a custom site template"
 $TenantName = "ecsrds"
 $SiteScriptJson = ".\SiteScriptRdsDemo.json"
+$ThumbnailUrl = "https://ecsrds.sharepoint.com/Freigegebene%20Dokumente/SiteTemplates/Thumbnails/RdsDemoSite.png"
+$PreviewImageUrl = "https://ecsrds.sharepoint.com/Freigegebene%20Dokumente/SiteTemplates/Thumbnails/RdsDemoSite.png"
 $SiteScriptJsonContent = Get-Content $SiteScriptJson -Raw
 
-Connect-SPOService -Url https://$TenantName-admin.sharepoint.com -Interactive
+Connect-PnPOnline -Url https://$TenantName-admin.sharepoint.com -Interactive
 
 #Creating a new Site Script & Site Design
 $SiteScript = Add-PnPSiteScript -Title $Title -Description $Description -Content $SiteScriptJsonContent
 Add-PnPSiteDesign -Title $Title -SiteScriptIds $SiteScript.Id -Description $Description -WebTemplate CommunicationSite
+
+# Update Site Design
+Get-PnPSiteDesign
+#...
+
+$SiteDesignId = "..."
+Set-PnPSiteDesign -Identity $SiteDesignId -PreviewImageUrl $PreviewImageUrl -ThumbnailUrl $ThumbnailUrl
